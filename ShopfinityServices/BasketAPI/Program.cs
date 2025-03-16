72df9de36b2c526c9cb935afefe6b2ec775265a7
Veljko Veljovic <veljko.veljovic@vegait.rs>
2025-03-16 13:10:50 +0100

    caching

--- ShopfinityServices/BasketAPI/Program.cs
+++ ShopfinityServices/BasketAPI/Program.cs
@@ -19,8 +19,12 @@ builder.Services.AddMarten(config =>
         })
     .UseLightweightSessions();
 
-builder.Services.AddScoped<IBasketRepository, BasketRepository>(); 
-
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
